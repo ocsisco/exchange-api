bb7b471ef0dcdd47fec9fb014a215a4355fc797f
Francisco <franciscogranellharo@gmail.com>
2023-10-15 19:35:26 +0200

    implement get_value in coin class

--- scrap.py
+++ scrap.py
@@ -13,77 +13,81 @@ class Coin:
         self.valueInDollars = float
         self.datetime = None
 
-    def get_value(self,source):
+    def __str__(self):
+        return self.name + " object coin"
 
-        try:
-            id_source= source[0]
-            name_coin= source[1]
-            url= source[2]
-            keyword= source[3]
-            init= source[4]
-            end= source[5]
+    def get_value(self,sources,tolerancePerCent):
 
-            r = requests.get(url)
-            datos = r.text
-            soup = BeautifulSoup(datos, "lxml")
-            soup = str(soup)
+        # Scrap values from sources and append in a list
+        all_values = []
 
-            keyword_position = soup.find(keyword)
-            value = soup[keyword_position + init : keyword_position + end]
+        for source in sources:
 
-            if "," in value:
-                value = float(value.replace(",", "."))
-            else:
-                value = float(value)
+            try:
+                id_source= source[0]
+                name_coin= source[1]
+                url= source[2]
+                keyword= source[3]
+                init= source[4]
+                end= source[5]
 
-            print(str(value))
+                r = requests.get(url)
+                datos = r.text
+                soup = BeautifulSoup(datos, "lxml")
+                soup = str(soup)
 
-            self.name = name_coin
-            self.valueInDollars = value
-            self.datetime = datetime.datetime.now()
+                keyword_position = soup.find(keyword)
+                value = soup[keyword_position + init : keyword_position + end]
 
-        except:
+                if "," in value:
+                    value = float(value.replace(",", "."))
+                else:
+                    value = float(value)
 
-            if __name__ == "__main__":
+                print(str(value))
+
+                all_values.append(value)
+
+            except:
                 print (Fore.RED+(str("This source with id:") + str(id_source) + " not contain valid value"))
                 print ("this value is " +Fore.YELLOW+ str(value) +Fore.RED+ " this is not a float" +Fore.RESET)
 
 
+        # Extract the preaverage of the all eur values list
+        total_value = 0.0
+        for value in all_values:
+            total_value = total_value + value
+        preaverage = total_value/len(all_values)
 
-def wrongValuesFilter(coins,name_coin,average,tolerance):
-    tolerance = tolerance
-    okaycoins = []
+        # Extract only coherent values, inside of the tolerance range
+        all_eur_values_normalized = []
+        tolerancePerCent = (tolerancePerCent*preaverage)/100
+        for value in all_values:
+            if value < preaverage + tolerancePerCent and value > preaverage - tolerancePerCent:
+                all_eur_values_normalized.append(value)
 
-    for coin in coins:
-        if coin.name == name_coin:
-            if coin.valueInDollars < average + tolerance and coin.valueInDollars > average - tolerance:
-                okaycoins.append(coin)
+        # Extract the average of the all eur values normalized
+        total_value = 0.0
+        for value in all_eur_values_normalized:
+            total_value = total_value + value
+        average = total_value/len(all_eur_values_normalized)
 
-    return okaycoins
+        coin = Coin()
+        coin.name = name_coin
+        coin.valueInDollars = average
+        coin.datetime = datetime.datetime.now()
 
+        return coin
+        
 
-
-def average(nameCoinSource):
-    all_eur_values = []
-    coin = Coin()
-    for source in nameCoinSource:
-        coin.get_value(source)
-        all_eur_values.append(coin.valueInDollars)
-
-    total_value = 0.0
-    for value in all_eur_values:
-        total_value = total_value + value
-    value = total_value/len(all_eur_values)
-
-    coin.valueInDollars = value
-    return coin
-
-
-eur = average(EUR)
-gbp = average(GBP)
+tolerancePerCent = 5
 
 
-
+eur = Coin()
+eur = eur.get_value(EUR,tolerancePerCent)
+print(eur)
+print("")
+print(eur.valueInDollars)
 
 
 
